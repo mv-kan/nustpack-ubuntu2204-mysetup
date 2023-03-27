97363cdc622efd6c328d9616f1fab5a3151cacac
Michael Kan <michael.kan.mail@gmail.com>
2023-03-27 17:17:30 +0200

    nust

--- install.bash
+++ install.bash
@@ -1,12 +1,13 @@
 #!/bin/bash
+RETRIES=10
 echo "WARNING close Firefox app, otherwise this script will fail"
-./nust aptupdate.bash
+./nust aptupdate.bash -r ${RETRIES}
 ./nust unsnap.bash
-./nust chrome.bash
-./nust vscode.bash
-./nust git.bash
-./nust xmind.bash
-./nust neovim.bash
-./nust gnome-console.bash
-./nust gnome-clipboard.bash
-./nust zsh.bash
+./nust chrome.bash -r ${RETRIES}
+./nust vscode.bash -r ${RETRIES} 
+./nust git.bash -r ${RETRIES}
+./nust xmind.bash -r ${RETRIES}
+./nust neovim.bash -r ${RETRIES}
+./nust gnome-console.bash -r ${RETRIES}
+./nust zsh.bash -r ${RETRIES} 
+./nust gnome-clipboard.bash -r ${RETRIES}
---
+++ nust.bash
@@ -0,0 +1,2 @@
+wget https://github.com/mv-kan/nust/releases/download/v2.0.1/nust-v2.0.1-linux-amd64.tar.gz -O nust.tar.gz
+tar -xzf nust.tar.gz
